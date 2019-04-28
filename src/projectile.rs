5470345179ca479f23423b9aff1e8ad1a700a580
kuviman <kuviman@gmail.com>
2019-04-28 11:48:54 +0400

    Projectile sparks

--- src/projectile.rs
+++ src/projectile.rs
@@ -2,6 +2,8 @@ use crate::*;
 
 pub struct Projectile {
     entity: Entity,
+    sparks: Vec<(f32, Entity)>,
+    next_spark: f32,
 }
 
 impl Deref for Projectile {
@@ -18,6 +20,10 @@ impl DerefMut for Projectile {
 }
 
 impl Projectile {
+    const SPARK_FREQ: f32 = 500.0;
+    const SPARK_MAX_SPEED: f32 = 5.0;
+    const SPARK_LIFE: f32 = 0.3;
+
     pub fn new(owner_id: Option<usize>, color: Color<f32>) -> Self {
         Self {
             entity: Entity {
@@ -27,14 +33,39 @@ impl Projectile {
                 vel: vec2(0.0, 0.0),
                 size: 0.0,
             },
+            sparks: Vec::new(),
+            next_spark: 0.0,
         }
     }
 
     pub fn update(&mut self, delta_time: f32) {
         self.entity.update(delta_time);
+        self.next_spark -= delta_time * self.mass();
+        while self.next_spark < 0.0 {
+            self.next_spark += 1.0 / Self::SPARK_FREQ;
+            self.sparks.push((
+                0.0,
+                Entity {
+                    owner_id: None,
+                    color: mix(Color::WHITE, self.entity.color),
+                    pos: self.entity.pos,
+                    vel: random_circle_point() * Self::SPARK_MAX_SPEED,
+                    size: self.entity.size / 2.0,
+                },
+            ))
+        }
+        for &mut (ref mut t, ref mut e) in &mut self.sparks {
+            *t += delta_time;
+            e.update(delta_time);
+            e.color.a = 1.0 - *t / Self::SPARK_LIFE;
+        }
+        self.sparks.retain(|&(t, _)| t < Self::SPARK_LIFE);
     }
 
     pub fn draw(&self, particles: &mut Vec<ParticleInstance>) {
         self.entity.draw(particles);
+        for &(_, ref e) in &self.sparks {
+            e.draw(particles);
+        }
     }
 }
